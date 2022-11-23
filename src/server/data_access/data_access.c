1d8cc8c01e50eb261a38e015830cc39fd3239d75
Abner Ferrari <aeliferrari@hotmail.com>
2022-11-22 22:34:52 -0300

    ajustes

--- src/server/data_access/data_access.c
+++ src/server/data_access/data_access.c
@@ -5,13 +5,15 @@
 #include <stdio.h>
 #include <string.h>
 
-void database_read(char* table, char* buffer, long buffer_size, int chunk_size, int metadata_size, pthread_mutex_t* mutex){
+pthread_mutex_t mutex_funcionarios_file = PTHREAD_MUTEX_INITIALIZER;
+
+void database_read(char* table, char* buffer, long buffer_size, int chunk_size, int metadata_size){
     int length = strlen(table) + strlen(FILE_EXTENSION);
     char file_name[length];
     get_file_name_with_extension(table, length, file_name);
 
     // Critical Session
-    pthread_mutex_lock(mutex);
+    pthread_mutex_lock(&mutex_funcionarios_file);
 
     FILE* file = fopen(file_name, "r");
     
@@ -23,49 +25,56 @@ void database_read(char* table, char* buffer, long buffer_size, int chunk_size, 
     Funcionario func = {};
     //fread(buffer, buffer_size, 1, file);
     int times = buffer_size / chunk_size;
+    char local_buffer[chunk_size + metadata_size];
+    //char* local_buffer = max_size;
+    //bzero(local_buffer, chunk_size + metadata_size);
+
     while(fread(&func, chunk_size, 1, file) != 0){
-        char local_buffer[chunk_size + metadata_size];
-        sprintf(local_buffer, FUNCIONARIO_PRETTY_FORMAT, func.nome, func.cpf, func.senha);
+        func.nome[FUNCIONARIO_NAME_SIZE -1] = '\0';
+        func.cpf[FUNCIONARIO_CPF_SIZE -1] = '\0';
+        func.senha[FUNCIONARIO_PASSWORD_SIZE -1] = '\0';
+        bzero(local_buffer, chunk_size);
+        sprintf(local_buffer, "Nome: %50s\nCPF: %11s\nSenha: %20s\n\n\n", func.nome, func.cpf, func.senha);
         strcat(buffer, local_buffer);
     }
     
     fclose(file);
 
-    pthread_mutex_unlock(mutex);
+    pthread_mutex_unlock(&mutex_funcionarios_file);
     // End Critical Session
 }
 
-int database_write(char* table_name, char* buffer, pthread_mutex_t* mutex)
+int database_write(char* table_name, char* buffer)
 {
-    printf("%s", buffer);
-    int length = strlen(table_name) + strlen(FILE_EXTENSION);
+    int length = strlen(table_name) + strlen(FILE_EXTENSION) + 1;
     char file_name[length];
     get_file_name_with_extension(table_name, length, file_name);
 
     // Critical Session
-    pthread_mutex_lock(mutex);
+    pthread_mutex_lock(&mutex_funcionarios_file);
 
     FILE* file = fopen(file_name, "a");
     Funcionario func = {};
     
     sscanf(buffer, FUNCIONARIO_FORMAT_OUT, func.nome, func.cpf, func.senha);
-    
-    // func.nome[FUNCIONARIO_NAME_SIZE - 1] = '\0';
-    // func.cpf[FUNCIONARIO_CPF_SIZE - 1] = '\0';
-    // func.senha[FUNCIONARIO_PASSWORD_SIZE - 1] = '\0';
 
     fprintf(file, FUNCIONARIO_FORMAT_IN, func.nome, func.cpf, func.senha);
     fclose(file);
 
-    pthread_mutex_unlock(mutex);
+    pthread_mutex_unlock(&mutex_funcionarios_file);
     
     // End Critical Session
 }
 
-int user_exists(char* buffer, pthread_mutex_t* mutex)
+void database_delete(char* table_name, char* key)
+{
+    
+}
+
+int user_exists(char* buffer)
 {
     // Critical Session
-    pthread_mutex_lock(mutex);
+    pthread_mutex_lock(&mutex_funcionarios_file);
 
     FILE* file = fopen("funcionarios.txt", "r");
     int found = 0;
@@ -87,9 +96,28 @@ int user_exists(char* buffer, pthread_mutex_t* mutex)
 
     fclose(file);
 
-    pthread_mutex_unlock(mutex);
+    pthread_mutex_unlock(&mutex_funcionarios_file);
     
     // End Critical Session
 
     return found;
+}
+
+long get_file_size(char* file_name)
+{ 
+    pthread_mutex_lock(&mutex_funcionarios_file);
+    int length = strlen(file_name) + FILE_EXTENSION_SIZE;
+    char file_name_with_extension[length];
+    get_file_name_with_extension(file_name, length, file_name_with_extension);
+    
+    FILE* file = fopen(file_name_with_extension, "r");
+    
+    fseek(file, 0, SEEK_END); 
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET); 
+
+    fclose(file);
+    pthread_mutex_unlock(&mutex_funcionarios_file);
+
+    return size;
 }
